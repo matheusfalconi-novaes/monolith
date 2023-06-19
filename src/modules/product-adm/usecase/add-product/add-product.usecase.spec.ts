3b8153637eb302fb3af4c432a29a474c815b96ea
Matheus Novaes <matheusfalcao.novaes@gmail.com>
2023-06-19 17:55:03 -0300

    add 'product-adm' module

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,30 @@
+import AddProductUseCase from "./add-product.usecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
+
+describe("Add Product use case unit test", () => {
+  it("should add a product", async () => {
+    const productRepository = MockRepository();
+    const usecase = new AddProductUseCase(productRepository);
+    const input = {
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+
+    const output = await usecase.execute(input);
+
+    expect(productRepository.add).toHaveBeenCalled();
+    expect(output.id).toBeDefined();
+    expect(output.name).toBe(input.name);
+    expect(output.description).toBe(input.description);
+    expect(output.purchasePrice).toBe(input.purchasePrice);
+    expect(output.stock).toBe(input.stock);
+  });
+});
